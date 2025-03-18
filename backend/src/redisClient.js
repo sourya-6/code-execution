24cca3408d47ce8b1b3ec7142a1a18a19e0928cd
sourya-6 <saisourya678@gmail.com>
2025-03-18 16:15:57 +0530

    done edits on redisclient

--- backend/src/redisClient.js
+++ backend/src/redisClient.js
@@ -1,10 +1,24 @@
-const redis = require("redis");
+// const redis = require("redis");
 
-const client = redis.createClient({
-    host: process.env.REDIS_HOST || "localhost",
-    port: process.env.REDIS_PORT || 6379,
+// const client = redis.createClient({
+//     host: process.env.REDIS_HOST || "localhost",
+//     port: process.env.REDIS_PORT || 6379,
+// });
+
+// client.on("error", (err) => console.error("Redis Error:", err));
+
+// module.exports = client;
+
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+    url: "redis://redis:6379" // This should match your Docker Redis service name
 });
 
-client.on("error", (err) => console.error("Redis Error:", err));
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
-module.exports = client;
+(async () => {
+    await redisClient.connect();
+})();
+
+module.exports = redisClient;
